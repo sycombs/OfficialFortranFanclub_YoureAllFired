e6b650e3c82556ec02e155b3a353ca656d0465d1
Matthew Showers <MDSTardis42@gmail.com>
2018-09-07 18:32:55 -0500

    tied command line interface to board

--- Board.py
+++ Board.py
@@ -17,12 +17,12 @@ class Board:
         self.height = height
         self.width = width
         self.mines = mines
-        self.board = []
+        self.grid = []
         for i in range(height):
             temp = []
             for j in range(width):
                 temp.append(Cell())
-            self.board.append(temp)
+            self.grid.append(temp)
         loc = []
         for i in range(height):
             for j in range(width):
@@ -31,29 +31,29 @@ class Board:
         for i in range(mines):
             row = loc[i][0]
             col = loc[i][1]
-            self.board[row][col].isBomb = True
+            self.grid[row][col].isBomb = True
             if row-1 >= 0:
-                self.board[row-1][col].adj += 1
+                self.grid[row-1][col].adj += 1
                 if col-1 >= 0:
-                    self.board[row-1][col-1].adj += 1
+                    self.grid[row-1][col-1].adj += 1
                 if col+1 < width:
-                    self.board[row-1][col+1].adj += 1
+                    self.grid[row-1][col+1].adj += 1
             if row+1 < height:
-                self.board[row+1][col].adj += 1
+                self.grid[row+1][col].adj += 1
                 if col-1 >= 0:
-                    self.board[row+1][col-1].adj += 1
+                    self.grid[row+1][col-1].adj += 1
                 if col+1 < width:
-                    self.board[row+1][col+1].adj += 1
+                    self.grid[row+1][col+1].adj += 1
             if col-1 >= 0:
-                self.board[row][col-1].adj += 1
+                self.grid[row][col-1].adj += 1
             if col+1 < width:
-                self.board[row][col+1].adj += 1
+                self.grid[row][col+1].adj += 1
 
     def displayBoard(self):
         for i in range(self.height):
             for j in range(self.width):
-                print(self.board[i][j], end="")
+                print(self.grid[i][j], end="")
             print()
 
-test = Board(10, 10, 1)
-test.displayBoard()
+# test = Board(10, 10, 1)
+# test.displayBoard()
--- Play.py
+++ Play.py
@@ -1,23 +1,68 @@
-#import Map
+from Board import Board
+
+def show(b):
+    print("  ",end="")
+    for i in range(b.width):
+        print(" "i, end="")
+    print("")
+    print("  ",end="")
+    for i in range(3*b.width):
+        print('-', end="")
+    print("")
+    for i in range(b.height):
+        print(i, end="")
+        print("|",end="")
+        for j in range(b.width):
+            if b.grid[i][j].isRevealed:
+                if b.grid[i][j].adj == 0:
+                    print(" _ ", end ='')
+                else:
+                    print(str(b.grid[i][j].adj), end ='')
+            elif b.grid[i][j].isFlagged:
+                print(" F ", end = '')
+            else:
+                print(" X ", end ='')
+        print()
 
 def run():
-
-    height = input("Enter the height of the board:")
-    width = input("Enter the width of the board:")
-    mines = input("Enter the number of mines:")
-    #board = Map(height, width, mines)
+    #TODO discuss max height/width???
+    height = int(input("Enter the height of the board:"))
+    width = int(input("Enter the width of the board:"))
+    mines = int(input("Enter the number of mines:"))
+    while mines >= height*width:
+        mines = input("please use less than " + str(height*width) + " mines.  Try again: ") 
+    board = Board(height, width, mines)
 
     lose = False
     while not lose:
-        action = input("What would you like to do? Enter 'r' to reveal or 'f' to flag:")
-        if action == "r": text = "reveal"
-        else: text = "flag"
-        row = input(f"For the cell you would like to {text}, enter row:")
-        column = input(f"For the cell you would like to {text}, enter column:")
+        # action = input("What would you like to do? Enter 'r' to reveal, 'f' to flag, or 's' to show:")
+        # if action == "r": text = "reveal"
+        # elif action == "f": text = "flag"
+        # else: text = ""
+        # row = input(f"For the cell you would like to {text}, enter row:")
+        # column = input(f"For the cell you would like to {text}, enter column:")
 
-        lose = click(row, column, action)
+        c = input("MENU \n Reveal: r x y\n Flag: f x y\n Show: s\n Quit: q \n <Prompt>: ")#TODO discuss possible promts, like turn counter
+        a = c.split()[0]
+        if a == "r" or a == "f":
+            column, row = c.split()[1],c.split()[2]
+            lose = click(board,int(row), int(column), a)
+        elif c == "s":
+            show(board)
+        elif c == "q":
+            break
+        else:
+            print("invalid Command")
+            continue
 
-def click(row, column, action):
-    return True
+def click(b,row, column, action):
+    if action == "r":
+        if b.grid[row][column].isBomb:
+            return True
+        else:
+            b.grid[row][column].isRevealed = True
+            return False
+    elif action == "f":
+        b.grid[row][column].isFlagged = True
 
 run()
