e4c37f593df0d4a71ae001e0b2f8edd619999fc8
tstrickler4 <strickler.tanner@gmail.com>
2018-09-07 20:20:02 -0500

    fix spacing issue

--- Play.py
+++ Play.py
@@ -1,27 +1,27 @@
 from Board import Board
 
 def show(b):
-    print("  ",end="")
+    print("   ", end="")
     for i in range(b.width):
-        print(" "i, end="")
-    print("")
-    print("  ",end="")
-    for i in range(3*b.width):
-        print('-', end="")
-    print("")
+        print(" "+f"{i:02}"+" ", end="")
+    print()
+    print("  ", end="")
+    for i in range(b.width):
+        print('----', end="")
+    print()
     for i in range(b.height):
-        print(i, end="")
+        print(f"{i:02}", end="")
         print("|",end="")
         for j in range(b.width):
             if b.grid[i][j].isRevealed:
                 if b.grid[i][j].adj == 0:
-                    print(" _ ", end ='')
+                    print("  _ ", end='')
                 else:
-                    print(str(b.grid[i][j].adj), end ='')
+                    print("  "+str(b.grid[i][j].adj)+" ", end='')
             elif b.grid[i][j].isFlagged:
-                print(" F ", end = '')
+                print("  F ", end='')
             else:
-                print(" X ", end ='')
+                print("  X ", end='')
         print()
 
 def run():
@@ -30,7 +30,7 @@ def run():
     width = int(input("Enter the width of the board:"))
     mines = int(input("Enter the number of mines:"))
     while mines >= height*width:
-        mines = input("please use less than " + str(height*width) + " mines.  Try again: ") 
+        mines = input("please use less than " + str(height*width) + " mines.  Try again: ")
     board = Board(height, width, mines)
 
     lose = False
@@ -42,11 +42,11 @@ def run():
         # row = input(f"For the cell you would like to {text}, enter row:")
         # column = input(f"For the cell you would like to {text}, enter column:")
 
-        c = input("MENU \n Reveal: r x y\n Flag: f x y\n Show: s\n Quit: q \n <Prompt>: ")#TODO discuss possible promts, like turn counter
+        c = input("MENU \n Reveal: r x y\n Flag: f x y\n Show: s\n Quit: q \n <Prompt>: ")#TODO discuss possible promts, like turn counter, check number of parameters in input
         a = c.split()[0]
         if a == "r" or a == "f":
             column, row = c.split()[1],c.split()[2]
-            lose = click(board,int(row), int(column), a)
+            lose = click(board, int(row), int(column), a)
         elif c == "s":
             show(board)
         elif c == "q":
@@ -55,7 +55,7 @@ def run():
             print("invalid Command")
             continue
 
-def click(b,row, column, action):
+def click(b, row, column, action):
     if action == "r":
         if b.grid[row][column].isBomb:
             return True
@@ -64,5 +64,6 @@ def click(b,row, column, action):
             return False
     elif action == "f":
         b.grid[row][column].isFlagged = True
+        return False
 
 run()
