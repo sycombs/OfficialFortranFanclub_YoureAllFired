a75c132bdb1daa3dbbda118560e13d2daa5c9dab
Nathan Pelletier <nathan.a.pelletier@gmail.com>
2018-09-28 14:16:32 -0500

    Add Time Class Skeleton

---
+++ Time.py
@@ -0,0 +1,25 @@
+import time
+class Time:
+    def __init__(self):
+        self.start = time.time()
+        self.end = time.time()
+        self.totalTime = self.totalTime = self.end-self.start
+    def game_over_time(self):
+        self.end = time.time()
+        self.totalTime = self.end-self.start
+    def show_total(self):
+        return self.totalTime
+    def compare_time(self, other):
+        if self.totalTime>other.showTotal:
+            print("You Win!")
+        else if self.totalTime<other.showTotal:
+            print("You Lose...")
+        else:
+            print("It's a draw?")
+    # def countDown(hour, min, sec):
+    #     self.min, self.sec = divmod(sec, 60)
+    #     self.hour, self.min = divmod(min, 60)
+    #     timedisplay = '{:02d}{:02d}:{02d}'.format(hour, min, sec)
+    #     print(timedisplay, end='\r')
+    #     time.sleep(1)
+    #     sec -= 1
