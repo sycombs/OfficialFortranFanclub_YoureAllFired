0b22663cb62e752bfaf5c620f37e9f2f102f7672
EDHidalgo <ehidalgo@ku.edu>
2018-10-01 09:50:11 -0500

    Added clarification for Server.py 's intended functionality

--- Server.py
+++ Server.py
@@ -1,15 +1,28 @@
 """@package docstring
-Server.py handles sending and receiving data, but based on that data
-it will load up the necessary rules to handle the game itself... bad comment
+Server.py contains all of the game server functionality. A server should
+    1) Be created
+    2) Listen for incoming messages
+    3) Interpret messages and call other code as necessary
+
+Server.py should not be used to run a server, any instances of a server
+should be created in Play.py . Server.py does not handle game logic either, it
+only facilitates the communication between the client (where the user interacts)
+and the server (where the game specific code is handled).
 """
 
-# Server testing
+''' TODO: Remove the instance of a server loop from here and place it elsewhere
+'''
+
+
+# Server Imports
 from socket import *
 import json
 
-
+# Server Data
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
+
+# Bind a server
 serverSocket.bind(('', serverPort))
 
 def interpret_data(data):
@@ -32,7 +45,9 @@ def interpret_data(data):
     except JSONDecodeError as e:
         print(e)
 
-''' SERVER LOOP '''
+
+
+''' SERVER LOOP - PLACE ELSEWHERE'''
 while True:
     # Receive the data
     data, clientAddress = serverSocket.recvfrom(2048 * 2 * 2 * 2)
