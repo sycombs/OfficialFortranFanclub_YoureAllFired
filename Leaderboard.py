3c8afeb45e01bd205d0120914c435754ecd483b7
Sydney Combs <sydneyycombs@gmail.com>
2018-10-03 10:53:02 -0500

    Leaderboard

---
+++ Leaderboard.py
@@ -0,0 +1,16 @@
+
+
+class Leaderboard:
+    #score variable
+
+    def get_leaderboard(self):
+        #return top 10...20 scores
+
+    def sort_leaderboard(self):
+        #sorts Leaderboard
+
+    def add_entry(self):
+        #add score to leaderboard
+
+    def clear(self):
+        #clear leaderboard
