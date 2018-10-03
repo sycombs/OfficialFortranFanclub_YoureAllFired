534a7a65da9fee3a9ff8b6c882a7411b1dc5fe9b
EDHidalgo <ehidalgo@ku.edu>
2018-10-03 09:51:29 -0500

    Wrote some logic for the CoOp gameplay loop

--- Server.py
+++ Server.py
@@ -8,6 +8,9 @@ Server.py should not be used to run a server, any instances of a server
 should be created in Play.py . Server.py does not handle game logic either, it
 only facilitates the communication between the client (where the user interacts)
 and the server (where the game specific code is handled).
+
+We may not run a pure client-server model so keep that in mind when moving
+code out of this file... like the server loop
 """
 
 ''' TODO: Remove the instance of a server loop from here and place it elsewhere
