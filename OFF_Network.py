f83fd9dc99e71aa22e9d8a087d00268052b855ef
EDHidalgo <ehidalgo@ku.edu>
2018-10-03 14:12:45 -0500

    Going to move over all shared client/server functionality to the OFF_Network module

---
+++ OFF_Network.py
@@ -0,0 +1,8 @@
+# Official Fortran Fanclub Network module
+
+# Socket allows us to create and deal with networks
+import socket
+
+# JSON and pickle allow us to serialize game data
+import json
+import pickle
