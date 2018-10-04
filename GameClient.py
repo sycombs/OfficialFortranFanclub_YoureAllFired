1a11816fce0ad062e542f947a25b62dc301b06b3
EDHidalgo <ehidalgo@ku.edu>
2018-10-04 10:42:13 -0500

    Minor clarification added to GameClient comments

--- GameClient.py
+++ GameClient.py
@@ -1,5 +1,8 @@
 """@package docstring
-GameClient.py
+GameClient.py deals with client specific functionality such as:
+    Update player data
+    Provide an interface for network communication
+    And... other stuff
 """
 
 # By importing OFF_Network we should have access to all of the server
