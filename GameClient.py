fa333e5456f21532962531f8a026ededf883a3e6
EDHidalgo <ehidalgo@ku.edu>
2018-10-04 13:29:25 -0500

    See the comments left in file

--- GameClient.py
+++ GameClient.py
@@ -8,6 +8,19 @@ GameClient.py deals with client specific functionality such as:
 # By importing OFF_Network we should have access to all of the server
 import OFF_Network
 
+
+''' Do we even need a separate receive_data() function? Currently all it does is
+just pings the server with 'hi' and then waits for a response. That's...
+pointless since the send_comm() function can be made to do that easily
+
+This will only be useful if we have it wait for data in a drastically different
+way?
+
+Instead we could just constantly ping the server with an update request. Just
+send it the string 'Update please' or some other nonsense and wait for a
+response
+'''
+
 def receive_data():
     clientSocket = socket(AF_INET, SOCK_DGRAM)
     clientSocket.sendto("hi".encode(), serverInfo)
