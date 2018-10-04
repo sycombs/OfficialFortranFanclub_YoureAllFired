92b0f9b9cbef2b2229dc8bb2426f1deea7db03eb
EDHidalgo <ehidalgo@ku.edu>
2018-10-04 10:19:43 -0500

    Removed duplicate data (server info) and duplicate code (send_data)

--- GameClient.py
+++ GameClient.py
@@ -2,45 +2,9 @@
 GameClient.py
 """
 
-''' TODO:
-    1) Include proper exceptions for try-catch blocks
-         Sections where this is required are flagged with # HERE
-    2) Change 'from socket import *' to 'import socket' and fix any resultant
-    issues
-    3) Remove hardcoded server data?
-'''
+# By importing OFF_Network we should have access to all of the server
+import OFF_Network
 
-
-# Server Data
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-
-# Store the data as a tuple (?) just to make things look cleaner later on
-serverInfo = (serverName, serverPort)
-
-def send_data(rawData):
-    """
-    send_data() will... not have to check the type of data we're sending?
-
-    It seems like the sockets module does a pretty good job of taking care of
-    sending data over regardless
-    """
-
-    try:
-        serializedData = json.dumps(self.rawData)
-    except Exception as e:
-        print(e)
-
-
-    # Create a socket
-    #clientSocket = socket(AF_INET, SOCK_DGRAM)
-
-    # Try sending the data, and if it doesn't work then print some error
-    try:
-        clientSocket.sendto(pData.encode(), serverInfo)
-        clientSocket.close()
-
-    except Exception as e:
-        print(e)
 
 def get_board():
     clientSocket = socket(AF_INET, SOCK_DGRAM)
