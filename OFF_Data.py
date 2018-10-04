2d899ad0a684228a835068d0242fbf0e9ee15f73
EDHidalgo <ehidalgo@ku.edu>
2018-10-04 10:48:26 -0500

    Created OFF_Data.py and removed the now duplicate functionality from OFF_Network.py

---
+++ OFF_Data.py
@@ -0,0 +1,37 @@
+"""@package docstring
+  OFF_Data.py will handle all the (de)serialization that's necessary so that
+  any changes to how we handle data is separated from the network code
+
+  Currently we use:
+    JSON
+    Python 3.7 Pickle
+"""
+
+''' SERIALIZATION FUNCTIONS '''
+
+def pickle_obj(serverSocket, clientAddress, rawData):
+    """
+    send_obj() sends pickled object data
+    """
+    try:
+        pickleData = pickle.dumps(rawData)
+        serverSocket.sendto(pickleData, clientAddress)
+    except Exception as e:
+        print(e)
+
+def send_json(serverSocket, clientAddress, rawData):
+    """
+    send_json()
+    """
+    try:
+        jsonData = json.dumps(rawData)
+        serverSocket.sendto(jsonData, clientAddress)
+    except Exception as e:
+        print(e)
+
+
+
+''' How do we best structure this?
+def serialize_data(rawData):
+    Call proper serialization function
+'''
--- OFF_Network.py
+++ OFF_Network.py
@@ -14,38 +14,6 @@ SERVER_PORT    = 12000
 
 SERVER_INFO = (SERVER_ADDRESS, SERVER_PORT)
 
-''' SERIALIZATION FUNCTIONS '''
-
-def pickle_obj(serverSocket, clientAddress, rawData):
-    """
-    send_obj() sends pickled object data
-    """
-    try:
-        pickleData = pickle.dumps(rawData)
-        serverSocket.sendto(pickleData, clientAddress)
-    except Exception as e:
-        print(e)
-
-def send_json(serverSocket, clientAddress, rawData):
-    """
-    send_json()
-    """
-    try:
-        jsonData = json.dumps(rawData)
-        serverSocket.sendto(jsonData, clientAddress)
-    except Exception as e:
-        print(e)
-
-
-
-''' How do we best structure this?
-def serialize_data(rawData):
-    Call proper serialization function
-'''
-
-
-
-
 ''' NETWORK COMMUNICATION FUNCTIONS '''
 
 def send_comm(data, sourceSocket, destination):
