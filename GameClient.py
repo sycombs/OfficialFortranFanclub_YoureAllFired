a382af25ee5f02c43299c98c17e24ef2df348e60
EDHidalgo <ehidalgo@ku.edu>
2018-10-04 13:25:37 -0500

    Removed GetBoard since we will start sending only the string representation of Board and will be turning that into a JSON object

--- GameClient.py
+++ GameClient.py
@@ -8,14 +8,6 @@ GameClient.py deals with client specific functionality such as:
 # By importing OFF_Network we should have access to all of the server
 import OFF_Network
 
-
-def get_board():
-    clientSocket = socket(AF_INET, SOCK_DGRAM)
-    clientSocket.sendto("hi".encode(), serverInfo)
-    serData, serverAddress = clientSocket.recvfrom(2048)
-
-    return serData
-
 def receive_data():
     clientSocket = socket(AF_INET, SOCK_DGRAM)
     clientSocket.sendto("hi".encode(), serverInfo)
